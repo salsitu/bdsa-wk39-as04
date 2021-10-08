d00652d08bbbf5a4ea61f8263c1412b47300cd2d
Gustav12dk00 <laugus11@hotmail.com>
2021-10-08 12:30:03 +0200

    Updated TaskRepository methods and Test Class

--- Assignment4.Entities.Tests/TaskRepositoryTests.cs
+++ Assignment4.Entities.Tests/TaskRepositoryTests.cs
@@ -1,10 +1,41 @@
 using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Assignment4.Entities.Tests
 {
-    public class TaskRepositoryTests
+    public class TaskRepositoryTests : IDisposable
     {
+        private readonly KanbanContext _context;
+        private readonly TaskRepository _repo;
 
+        public TaskRepositoryTests()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            var builder = new DbContextOptionsBuilder<KanbanContext>().UseSqlite(connection);
+            var context = new KanbanContext(builder.Options);
+            context.Database.EnsureCreated();
+
+            // Insert Data
+            
+            // -----------
+
+            _context = context;
+            _repo = new TaskRepository(_context);
+
+        }
+
+        [Fact]
+        public void Create_creates_new_Task_with_generated_id()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
     }
 }
